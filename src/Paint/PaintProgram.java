dfb996ca220a1f66e7975d45e1045f5bf151231e
Zhasik@.com <125801424+Zhas1k@users.noreply.github.com>
2023-05-06 01:10:36 +0600

    Paint circle and square ne dokanca

--- src/Paint/PaintProgram.java
+++ src/Paint/PaintProgram.java
@@ -8,13 +8,13 @@ import javax.swing.event.*;
 public class PaintProgram extends JFrame {
     private int x, y;
     private Color color = Color.BLACK;
-    private JButton colorButton, clearButton, fillButton;
+    private JButton colorButton, clearButton, fillButton, circleButton, squareButton;
     private JSlider widthPen;
     private int widthLine = 1;
     private JPanel doska;
     public PaintProgram() {
         setTitle("Paint Program");
-        setSize(1500, 1000);
+        setSize(2500, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         colorButton = new JButton("Color");
@@ -72,14 +72,31 @@ public class PaintProgram extends JFrame {
                 y = e.getY();
             }
         });
-
+        circleButton = new JButton("Oval");
+        circleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Graphics g = doska.getGraphics();
+                g.setColor(color);
+                g.fillOval(x, y, widthLine, widthLine);
+            }
+        });
+        squareButton = new JButton("Square");
+        squareButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Graphics g = doska.getGraphics();
+                g.setColor(color);
+                g.fillRect(x, y,widthLine+x, widthLine+y);
+            }
+        });
 
 
         Container knopkaOryndary = getContentPane();
-        JPanel knopka = new JPanel(new GridLayout(3, 2));
+        JPanel knopka = new JPanel(new GridLayout(3, 3));
         knopka.add(colorButton);
         knopka.add(clearButton);
         knopka.add(fillButton);
+        knopka.add(circleButton);
+        knopka.add(squareButton);
         knopka.add(new JLabel("Line Width:"));
         knopka.add(widthPen);
         knopkaOryndary.add(knopka, BorderLayout.NORTH);
